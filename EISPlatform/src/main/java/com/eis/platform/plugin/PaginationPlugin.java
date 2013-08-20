0922571c5abf7f5af63c21a1b33c80c87efe4ec3
yunchow <yunchow@126.com>
2013-08-20 17:06:49 +0800

    9

--- EISPlatform/src/main/java/com/eis/platform/plugin/PaginationPlugin.java
+++ EISPlatform/src/main/java/com/eis/platform/plugin/PaginationPlugin.java
@@ -91,6 +91,16 @@ public class PaginationPlugin implements InitializingBean, Interceptor {
 		Assert.hasLength(dialect, "dialect must have length");
 		Assert.notEmpty(dialectMap, "dialectMap must not be empty");
 	}
+	
+	protected String getTotalCountStatementId(final MappedStatement mappedStatement) {
+		String pageStatementId = mappedStatement.getId();
+		String ns = "";
+		int index = pageStatementId.lastIndexOf(".");
+		if (index >= 0) {
+			ns = pageStatementId.substring(0, index);
+		}
+		return ns + ".findTotalCount";
+	}
 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
@@ -98,8 +108,8 @@ public class PaginationPlugin implements InitializingBean, Interceptor {
 		final BoundSql boundSql = statementHandler.getBoundSql();
 		
 		MetaObject metaStatementHandler = MetaObject.forObject(statementHandler, objectFactory, objectWrapperFactory);
-		
 	    final MappedStatement mappedStatement = (MappedStatement) metaStatementHandler.getValue("delegate.mappedStatement");
+	    
 	    if (mappedStatement.getId().indexOf("ByPage") == -1) {
 	    	return invocation.proceed();
 	    }
@@ -121,56 +131,26 @@ public class PaginationPlugin implements InitializingBean, Interceptor {
 			logger.debug("pageSize = {}", pageSize);
 			logger.debug("pageNo = {}", pageNo);
 		}
+		
+		String totalCountStatementId = getTotalCountStatementId(mappedStatement);
 		final Configuration configuration = mappedStatement.getConfiguration();
-		String pageStatementId = mappedStatement.getId();
-		String ns = "";
-		int index = pageStatementId.lastIndexOf(".");
-		if (index >= 0) {
-			ns = pageStatementId.substring(0, index);
-		}
-		String totalCountStatementId = ns + ".findTotalCount";
 		
 		// query for records total count
 		final MetaObject recordMetaObject = MetaObject.forObject(boundSql, objectFactory, objectWrapperFactory);
 		@SuppressWarnings("unchecked")
 		List<ParameterMapping> parameterMappings = (List<ParameterMapping>) recordMetaObject.getValue("parameterMappings");
 		MappedStatement totalMappedStatement = null;
+		
 		try {
 			totalMappedStatement = configuration.getMappedStatement(totalCountStatementId);
 		} catch (Exception e) {
 			logger.warn("cannot find target statement: {}", totalCountStatementId);
 		}
+		
 		if (totalMappedStatement == null) {
 			logger.error("statement [{}] is required", totalCountStatementId);
 			throw new IllegalStateException(String.format("cannot find required statement: {%s}", totalCountStatementId));
-			/*logger.warn("cannot find findTotalCount statement, default sql will be used");
-			SqlSource sqlSource = new SqlSource() {
-				
-				@Override
-				public BoundSql getBoundSql(Object parameterObject) {
-					String sql = "select count(*) " + boundSql.getSql().substring(boundSql.getSql().indexOf("from"));
-					int index = sql.indexOf("order");
-					if (index >= 0) {
-						sql = sql.substring(0, index);
-					}
-					BoundSql totalBoundSql = new BoundSql(configuration, sql, new ArrayList<ParameterMapping>(0), parameterObject);
-					MetaObject totalMetaObject = MetaObject.forObject(totalBoundSql, objectFactory, objectWrapperFactory);
-					List<ParameterMapping> paramList = new ArrayList<ParameterMapping>(parameterMappings.size());
-					for (ParameterMapping paramMapping : parameterMappings) {
-						ParameterMapping param = new ParameterMapping.Builder(configuration, paramMapping.getProperty(), paramMapping.getJavaType()).build();
-						paramList.add(param);
-					}
-					totalMetaObject.setValue("parameterMappings", paramList);
-					return totalBoundSql;
-				}
-			};
-			Builder builder = new Builder(configuration, totalCountStatementId, sqlSource, SqlCommandType.SELECT);
-			ResultMap resultMap = new ResultMap.Builder(configuration, "_ID_" + System.currentTimeMillis(), Integer.class, new ArrayList<ResultMapping>(0)).build();
-			List<ResultMap> resultMaps = new ArrayList<ResultMap>(1);
-			resultMaps.add(resultMap);
-			builder.resultMaps(resultMaps);
-			totalMappedStatement = builder.build();
-			configuration.addMappedStatement(totalMappedStatement);*/
+			// createMappedStatement(mappedStatement);
 		}
 		else {
 			logger.debug("findTotalCount param = {}", param);
@@ -179,6 +159,22 @@ public class PaginationPlugin implements InitializingBean, Interceptor {
 			param.put("total", total);
 		}
 		
+		recordMetaObject.setValue("parameterMappings", rebuildParameterMappings(configuration, parameterMappings));
+		
+	    Dialect adialect = dialectMap.get(dialect);
+	    String sql = adialect.buildPaginationSql(boundSql.getSql(), pageSize, pageNo);
+	    metaStatementHandler.setValue("delegate.boundSql.sql", sql);
+	    
+	    return invocation.proceed();		
+	}
+
+	/**
+	 * @param configuration
+	 * @param parameterMappings
+	 * @return
+	 */
+	protected List<ParameterMapping> rebuildParameterMappings(final Configuration configuration, List<ParameterMapping> parameterMappings) {
+		Assert.notNull(parameterMappings);
 		List<ParameterMapping> paramList = new ArrayList<ParameterMapping>(parameterMappings.size() + 2);
 		for (ParameterMapping paramMapping : parameterMappings) {
 			ParameterMapping paramMappingLocal = new ParameterMapping.Builder(configuration, paramMapping.getProperty(), paramMapping.getJavaType()).build();
@@ -188,14 +184,42 @@ public class PaginationPlugin implements InitializingBean, Interceptor {
 		paramList.add(parameterMapping);
 		parameterMapping = new ParameterMapping.Builder(configuration, "pageSize", Integer.class).build();
 		paramList.add(parameterMapping);
-		
-		recordMetaObject.setValue("parameterMappings", paramList);
-		
-	    Dialect adialect = dialectMap.get(dialect);
-	    String sql = adialect.buildPaginationSql(boundSql.getSql(), pageSize, pageNo);
-	    metaStatementHandler.setValue("delegate.boundSql.sql", sql);
-	    
-	    return invocation.proceed();		
+		return paramList;
+	}
+	
+	/**
+	 * crate dynamic find total count mapped statement
+	 * @param mappedStatement
+	 */
+	protected void createMappedStatement(final MappedStatement mappedStatement) {
+		/*logger.warn("cannot find findTotalCount statement, default sql will be used");
+		SqlSource sqlSource = new SqlSource() {
+			
+			@Override
+			public BoundSql getBoundSql(Object parameterObject) {
+				String sql = "select count(*) " + boundSql.getSql().substring(boundSql.getSql().indexOf("from"));
+				int index = sql.indexOf("order");
+				if (index >= 0) {
+					sql = sql.substring(0, index);
+				}
+				BoundSql totalBoundSql = new BoundSql(configuration, sql, new ArrayList<ParameterMapping>(0), parameterObject);
+				MetaObject totalMetaObject = MetaObject.forObject(totalBoundSql, objectFactory, objectWrapperFactory);
+				List<ParameterMapping> paramList = new ArrayList<ParameterMapping>(parameterMappings.size());
+				for (ParameterMapping paramMapping : parameterMappings) {
+					ParameterMapping param = new ParameterMapping.Builder(configuration, paramMapping.getProperty(), paramMapping.getJavaType()).build();
+					paramList.add(param);
+				}
+				totalMetaObject.setValue("parameterMappings", paramList);
+				return totalBoundSql;
+			}
+		};
+		Builder builder = new Builder(configuration, totalCountStatementId, sqlSource, SqlCommandType.SELECT);
+		ResultMap resultMap = new ResultMap.Builder(configuration, "_ID_" + System.currentTimeMillis(), Integer.class, new ArrayList<ResultMapping>(0)).build();
+		List<ResultMap> resultMaps = new ArrayList<ResultMap>(1);
+		resultMaps.add(resultMap);
+		builder.resultMaps(resultMaps);
+		totalMappedStatement = builder.build();
+		configuration.addMappedStatement(totalMappedStatement);*/
 	}
 
 	@Override
