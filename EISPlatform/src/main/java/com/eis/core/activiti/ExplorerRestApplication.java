606341f526d962df05b2ba10e5de9338ab63771c
yunchow <yunchow@126.com>
2013-09-05 19:16:19 +0800

    88

--- EISPlatform/src/main/java/com/eis/core/activiti/ExplorerRestApplication.java
+++ EISPlatform/src/main/java/com/eis/core/activiti/ExplorerRestApplication.java
@@ -9,22 +9,23 @@ import org.restlet.Restlet;
 import org.restlet.routing.Router;
 
 public class ExplorerRestApplication extends ActivitiRestApplication {
-  
-  public ExplorerRestApplication() {
-    super();
-  }
-  /**
-   * Creates a root Restlet that will receive all incoming calls.
-   */
-  @Override
-  public synchronized Restlet createInboundRoot() {
-    Router router = new Router(getContext());
-    router.attachDefault(DefaultResource.class);
-    ModelerServicesInit.attachResources(router);
-    DiagramServicesInit.attachResources(router);
-    JsonpFilter jsonpFilter = new JsonpFilter(getContext());
-    jsonpFilter.setNext(router);
-    return jsonpFilter;
-  }
+
+	public ExplorerRestApplication() {
+		super();
+	}
+
+	/**
+	 * Creates a root Restlet that will receive all incoming calls.
+	 */
+	@Override
+	public synchronized Restlet createInboundRoot() {
+		Router router = new Router(getContext());
+		router.attachDefault(DefaultResource.class);
+		ModelerServicesInit.attachResources(router);
+		DiagramServicesInit.attachResources(router);
+		JsonpFilter jsonpFilter = new JsonpFilter(getContext());
+		jsonpFilter.setNext(router);
+		return jsonpFilter;
+	}
 
 }
