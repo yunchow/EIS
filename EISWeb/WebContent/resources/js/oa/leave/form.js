43cf967a3901abe8fb17d77c3a925c882c12658e
yunchow <yunchow@126.com>
2013-09-06 11:07:25 +0800

    i

--- EISWeb/WebContent/resources/js/oa/leave/form.js
+++ EISWeb/WebContent/resources/js/oa/leave/form.js
@@ -2,10 +2,6 @@ jQuery.ns("oa.leave.form");
 
 jQuery.define(oa.leave.form, base, {
 	dgId: "leaveTimeDetailGrid",
-	searchField: "name",
-	deleteUrl: "sysmanage/user/delete/",
-    saveUrl: "sysmanage/user/save.htm",
-    updateUrl: "sysmanage/user/update.htm",
     
     init: function() {
     	context.log("oa.leave.form.init called");
